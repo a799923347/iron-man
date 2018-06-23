d9ce26e6195febd8e3e2a95fc17d122fd5f9b1eb
shachi <shachi@wacai.com>
2018-06-24 02:29:23 +0800

    feature:初始化项目

---
+++ iron-man-service/src/main/java/com/bowen/ironmanservice/IronManServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bowen.ironmanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronManServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronManServiceApplication.class, args);
+	}
+}
